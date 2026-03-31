ad18b323609af775a09f5da0bba7acfacb8458ef
DumontALINE <aline.dmnt83@gmail.com>
2026-03-31 09:09:44 +0200

    feat: ajout du detection du mouvement

---
+++ Untitled-Fishing-Game/components/move/useAccelerometer.tsx
@@ -0,0 +1,22 @@
+import {useState, useEffect, useRef} from "react";
+import {Accelerometer} from "expo-sensors";
+
+export default function useAccelerometer() {
+  const [{x, y, z}, setData] = useState({x:0, y:0, z:0});
+  const [magnitude, setMagnitude] = useState<number>(0);
+
+  useEffect(() => {
+    const subscription = Accelerometer.addListener(setData);
+    return ()=> subscription.remove();
+  }, []);
+
+  useEffect(() => {
+    const NewMagnitude = Math.sqrt(x**2 + y**2 + z**2);
+    setMagnitude(NewMagnitude)
+    if (NewMagnitude >= 3){
+      console.log("canne a pêche lancer");
+    }
+    }, [x,y,z]);
+
+  return { x, y, z, magnitude};
+}
